adaee983634d505d76cefba7eb4fa95bf8592763
Colby Canselo <colbycanselo@gmail.com>
2023-03-02 16:29:59 +0000

    OUTER JOIN

---
+++ problem1.sql
@@ -0,0 +1,1 @@
+SELECT * FROM class AS c LEFT OUTER JOIN student AS s ON c.class_title = s.class_title;
---
+++ problem2.sql
@@ -0,0 +1,1 @@
+SELECT * FROM class AS c RIGHT OUTER JOIN textbook AS t ON c.class_title = t.class_title;
